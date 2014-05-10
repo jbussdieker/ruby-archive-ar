c2b7e027e4ecf429f540c773ecd2d08e5fe735a1
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-05-10 14:58:15 -0700

    Split up logic in header_to_s

--- lib/archive/ar/format/bsd.rb
+++ lib/archive/ar/format/bsd.rb
@@ -24,28 +24,33 @@ module Archive
 
           def build_header(file)
             header = read_file_header(file)
+            header = adjust_header(header)
             header_to_s(header)
           end
 
           private
 
+          def adjust_header(header)
+            if header[:name].length > 16
+              namebuf = header[:name]
+              namebuf += "\0" * (4 - namebuf.length % 4) if (namebuf.length % 4) != 0
+              header[:size] += namebuf.length
+              header[:long_name] = namebuf
+              header[:name] = "#1/#{namebuf.length}"
+            end
+            header
+          end
+
           def header_to_s(header)
             data = ""
-            namebuf = header[:name]
-            if namebuf.length > 16
-              namebuf += "\0" * (4 - namebuf.length % 4) if (namebuf.length % 4) != 0
-              data += "%-16s" % "#1/#{namebuf.length}"
-              header[:size] += namebuf.length
-            else
-              data += "%-16s" % header[:name]
-            end
+            data += "%-16s" % header[:name]
             data += "%-12s" % header[:modified]
             data += "%-6s" % header[:owner]
             data += "%-6s" % header[:group]
             data += "%-8s" % header[:mode].to_s(8)
             data += "%-10s" % header[:size]
             data += "%2s" % header[:magic]
-            data += namebuf if header[:name].length > 16
+            data += header[:long_name] if header[:long_name]
             data
           end
         end
